3a29eff83bda14dd3d6baa922c0332b93d4200eb
luutuanan2003 <132368655+luutuanan2003@users.noreply.github.com>
2024-12-13 13:58:15 +0700

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by An Luu on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
